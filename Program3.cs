652743be8ba97b18e892f52fe6a3f6e0a4f1d0d9
victorperez200 <78993279+victorperez200@users.noreply.github.com>
2021-03-21 19:24:40 -0400

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Herencia3
+{
+    public class A
+    {
+        public A()
+        {
+            Console.WriteLine("Constructor de la clase A");
+        }
+    }
+
+    public class B : A 
+    {
+        public B()
+        {
+            Console.WriteLine("Constructor de la clase B");
+        }
+    }
+
+    public class C : B
+    {
+        public C()
+        {
+            Console.WriteLine("Constructor de la clase C");
+        }
+    }
+
+   class Prueba
+    {
+        static void Main(string[] args)
+        {
+            C obj1 = new C();
+            Console.ReadKey();
+        }
+    }
+}
